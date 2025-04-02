836ef302f855d4b02dfc3359a4a66867ef069d08
CompSciHigh24 <csh-github@compscihigh.org>
2025-04-02 08:20:36 -0400

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,53 @@
+// add YOUR baseURL and make sure it's public
+const baseURL = "https://turbo-train-q79vprg6vw69f47vj-3000.app.github.dev";
+
+// Get all menu items
+async function getAllMenuItems() {
+  const res = await fetch(baseURL + 'menus');
+  const data = await res.json();
+  console.log("All Menu Items:", data);
+}
+// uncomment to test GET route
+// getAllMenuItems();
+
+
+// Add a new menu item
+async function addNewItem(name, cost, rating, review) {
+  const res = await fetch(baseURL + '/menu/new', {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, cost, rating, review }),
+  });
+  console.log("New Item Added:", res);
+}
+
+// uncomment to test POST route
+// addNewItem("Fries", 3.5, 4, "Crispy and golden");
+
+
+// Update an item by name (only cost in this example)
+async function updateItemCost(name, newCost) {
+  const res = await fetch(baseURL + '/menu/update/' + name, {
+    method: "UPDATE",
+    headers: { "Content-Type": "application/json" },
+    body: { cost: newCost }
+  });
+  const data = await res.json();
+  console.log("Updated Item:", data);
+}
+
+// uncomment to test PATCH route
+// updateItemCost("Fries", 2.99);
+
+
+// Delete an item by name
+function deleteItem(name) {
+  const res = await fetch(baseURL + '/delete/' + name, {
+    method: "DELETE",
+  });
+  const data = await res.json();
+  console.log("Deleted Item:", data);
+}
+
+// uncomment to test DELETE route
+// deleteItem("Fries");
